7ddf3a4c82aaeadd41ee8f007ff3ee4ebb14545f
edsakamoto <42415029+edsakamoto@users.noreply.github.com>
2022-11-07 21:34:29 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+// Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+
+function App() {
+  // Aqui você irá criar os Estados para manipular os Inputs
+
+
+  return (
+    <div className="App">
+     <h1>Carga de estudiantes</h1>
+     <form>
+      {/* Comece a desenvolver o seu Código por aqui :) 
+      typecolor ou typetext*/}
+
+     </form>
+    </div>
+  )
+}
+
+export default App
